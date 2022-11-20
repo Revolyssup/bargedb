2756978bb985990ec3b54321135c36547d636f26
revolyssup <ashishjaitiwari15112000@gmail.com>
2022-11-21 01:13:11 +0530

    Add states on Consensus instance
    Signed-off-by: revolyssup <ashishjaitiwari15112000@gmail.com>

--- pkg/consensus/consensus.go
+++ pkg/consensus/consensus.go
@@ -4,10 +4,22 @@ import (
 	"github.com/Revolyssup/bargedb/pkg/log"
 	"github.com/Revolyssup/bargedb/pkg/store"
 	"github.com/Revolyssup/bargedb/pkg/transport"
+	"github.com/google/uuid"
 )
 
 type Instance struct {
-	Transport transport.Transport
-	Log       log.Instance
-	Store     store.Storage
+	Transport transport.Transport //The transport layer
+	Store     store.Storage       //The state machine for this RAFT
+	//persisted on non-volatile storage
+	currentTerm int       //latest term server has seen (initialized to 0 on first boot, increases monotonically)
+	votedFor    uuid.UUID //candidateId that received vote in current term (or null if none)
+	Log         log.Instance
+
+	//volatile state
+	commitIndex int //index of highest log entry known to be committed (initialized to 0, increases monotonically)
+	lastApplied int //index of highest log entry applied to state machine (initialized to 0, increases monotonically)
+
+	//volatile states only for leaders
+	nextIndex  map[uuid.UUID]log.Index //for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
+	matchIndex map[uuid.UUID]log.Index
 }
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,6 +1,12 @@
 package log
 
 type Instance struct {
+	Entries []Entry
+}
+type Index int
+type Entry struct {
+	Key   string
+	Value []byte
 }
 
 func (i *Instance) Write(key string, value interface{}) {}

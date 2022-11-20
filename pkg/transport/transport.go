389297d5030c382c49df43e5e45494ba61ae07bd
revolyssup <ashishjaitiwari15112000@gmail.com>
2022-11-21 01:55:01 +0530

    Add details
    Signed-off-by: revolyssup <ashishjaitiwari15112000@gmail.com>

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -1,5 +1,10 @@
 package transport
 
+import (
+	"github.com/Revolyssup/bargedb/pkg/log"
+	"github.com/google/uuid"
+)
+
 /*
 1. Transport layer maps a peer UUID(at consensus layer) to an actual address. For RPCs this will be the address of the RPC server while at the same time accepts requests as an RPC server.
 2. Internally for mocking, the implementation can just be a mapping of peer id to the appropriate go routine in which the peer's transport instance is running.
@@ -8,8 +13,29 @@ package transport
 */
 type Transport interface {
 	//For senders
-	AppendEntries()
-	RequestVote()
+	/*
+		Arguments:
+			term: leader’s term
+			leaderId: so follower can redirect clients
+			prevLogIndex: index of log entry immediately preceding new ones
+			prevLogTerm: term of prevLogIndex entry
+			entries[]: log entries to store (empty for heartbeat; may send more than one for efficiency) leaderCommit leader’s commitIndex
+		Results:
+			term: currentTerm, for leader to update itself
+			success: true if follower contained entry matching prevLogIndex and prevLogTerm
+	*/
+	AppendEntries(term int, leaderID uuid.UUID, prevLogIndex log.Index, prevLogEntry int, entries []log.Entry, leaderCommit int) (int, bool)
+	/*
+		Arguments:
+			term: candidate’s term
+			candidateId: candidate requesting vote
+			lastLogIndex: index of candidate’s last log entry (§5.4)
+			lastLogTerm: term of candidate’s last log entry (§5.4)
+		Results:
+			term: currentTerm, for candidate to update itself
+			voteGranted: true means candidate received vote
+	*/
+	RequestVote(term int, candidateID uuid.UUID, lastLogIndex log.Index, lastLogTerm int) (int, bool)
 	//For recievers
 	Listen()
 }

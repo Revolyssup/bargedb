2a2622753d7866c2de7bb26f13c5a20c729774ef
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2022-11-27 18:38:18 +0530

    add index entry
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,7 +1,19 @@
 package log
 
+import (
+	"encoding/gob"
+	"os"
+	"sync"
+)
+
+type IndexEntry struct {
+	StartingByte int64
+	EndingByte   int64
+}
 type Instance struct {
-	Entries []Entry
+	indexFile *os.File
+	dataFile  *os.File
+	mx        sync.Mutex
 }
 type Index int
 type Entry struct {
@@ -9,4 +21,19 @@ type Entry struct {
 	Value []byte
 }
 
-func (i *Instance) Write(key string, value interface{}) {}
+func (i *Instance) Write(key string, value []byte) error {
+	entry := Entry{
+		Key:   key,
+		Value: value,
+	}
+	i.mx.Lock()
+	defer i.mx.Unlock()
+	enc := gob.NewEncoder(i.dataFile)
+	err := enc.Encode(entry)
+	if err != nil {
+		return err
+	}
+	// var d gob.Decoder
+	// d.Decode()
+	return nil
+}
